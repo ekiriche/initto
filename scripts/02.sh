a6e6bdbf3221c92515e836fc16333dcd4615982e
kekcheburek <kirichevg@gmail.com>
2018-02-18 12:12:05 +0000

    asd

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,1 +1,5 @@
-(sudo apt-get update; sudo apt-get upgrade) >> /home/user/initto/update_script.log
+sudo touch /var/log/update_script.log
+log=$(sudo apt-get update && sudo apt-get upgrade)
+echo "$log" | sudo tee /var/log/update_script.log
+
+echo "0 4 * * 1 sh ~/initto/scripts/02.sh" | crontab -
